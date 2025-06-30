7b249644a88a4ff022e3b60626c2675ef4d743fe
Youssef Chiesi <355249@studenti.unimore.it>
2025-06-30 10:59:56 +0200

    diocane

--- prova_1.cpp
+++ prova_1.cpp
@@ -7,18 +7,7 @@ int somma (int n1, int n2)
 }
 int main()
 {
-    int a = 5;
-    int b = 3;
-    cout<<"MI TROVO IN BRANCH C"<<endl;
     
-    cout<<"questa è la enorme differenza:"<<endl;
-    int martina = 19;
-
-    int array[10];
-    for(int i = 0; i< 10; i++)
-    {
-        array[i] = i;
-    }
-    cout<< "Valore: "<<somma(a,b)<<endl;
+    cout<<"gkngdkaosgjnioasdjgijsajgilòasdighsaildòhgdas";
     return 0;
 }
